afa40949b4db675001d7d490338a267aa2689a3c
Ahsan Habib Hamim <youremail@example.com>
2026-01-03 12:19:28 +0600

    Final Update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,39 @@
-self.addEventListener("install",e=>{
+const CACHE_NAME = "smartkharch-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./lang.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("kharch").then(c=>c.addAll([
-      "./","index.html","style.css","app.js","lang.js"
-    ]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    }).catch(() => caches.match("./index.html"))
   );
 });
